0cdaabb21e7c75b0a3e236725736eea743899f1a
sthompson <swt25501@gmail.com>
2018-10-22 13:09:59 -0500

    Still working on trying to figure out how to rotate the base of the pyramid around its vertex.

--- Cubelet.py
+++ Cubelet.py
@@ -1,5 +1,5 @@
 from vpython import *
-import numpy as np
+import math
 
 """ Cubelet object class. Accepts a position of a 3-Dimensional vector 
     in the form of an (x, y , z) triplet containing a range of values from -1 to 1 """
@@ -7,32 +7,19 @@ import numpy as np
 
 class Cubelet(object):
 
-    def __init__(self, x, y, z, color):
-        self.pos = {"x": x, "y": y, "z": z}
-        self.red = color[0]
-        self.green= color[1]
-        self.blue= color[2]
-        cubelet = pyramid(pos=vector(self.pos["x"], self.pos["y"], self.pos["z"]), color=vector(self.red, self.green, self.blue))
+    def __init__(self, pos, size, color):
+        self.pos = {"x": pos[0], "y": pos[1], "z": pos[2]}
+        self.size = {"length": size[0], "width": size[1], "height": size[2]}
+        self.color = {"red" : color[0], "green" : color[1], "blue" : color[2]}
+        cubelet = pyramid(pos=vector(self.pos["x"], self.pos["y"], self.pos["z"]),
+                          color=vector(self.color["red"] , self.color["green"], self.color["blue"]),
+                          size=vector(self.size["length"], self.size["width"], self.size["height"]))
 
-        if self.pos["y"] == 1:
-            # print(2 + 2)
-            cubelet.rotate(np.radians(270))
-
-
-Cubelet(-1, -1, -1, (255, 0, 0))
-Cubelet(0, -1, -1, (255, 0, 0))
-Cubelet(1, -1, -1, (255, 0, 0))
-Cubelet(-1, 0, -1, (0, 255, 0))
-Cubelet(0, 0, -1, (0, 255, 0))
-Cubelet(1, 0, -1, (0, 255, 0))
-Cubelet(-1, 1, -1, (0, 0, 255))
-Cubelet(0, 1, -1, (0, 0, 255))
-Cubelet(1, 1, -1, (0, 0, 255))
+        self.vertex = size[2]
+        cubelet.rotate(angle=math.radians(270), axis=vec(self.vertex, 0, 0))
 
 
-
-
-
+Cubelet((0, 0, 0), (1, 1, 1), (0, 0, 255))
 
 
 

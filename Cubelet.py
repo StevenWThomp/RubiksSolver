db266769aada11f75b825e330f613087db6dee37
sthompson <swt25501@gmail.com>
2018-11-02 12:18:42 -0500

    I have no idea where to go and I'm tired of staring at the screen. I will come back when I have more experience.

--- Cubelet.py
+++ Cubelet.py
@@ -3,29 +3,30 @@ import math
 
 """ Cubelet object class. Accepts a position, size, and color argument. Each in the form of a 3-dimensional vector 
                                                         Arguments:
-                                    pos: (x, y, z) values ranging from -1 to 1
-                                    color: (0-255, 0-255, 0-255) Accepts a color values in the form of an RGB triplet
+                                    pos: (x, y, z) values ranging from -1 to 1.
+                                    color: (0-255, 0-255, 0-255) Accepts a color values in the form of an RGB triplet.
                                     OPTIONAL - size: (Length, Width, Height) Default values are (1, 1, 1) """
 
 
 class Cubelet(object):
 
-    def __init__(self, pos, color, size=None):
+    def __init__(self, pos, color):
 
-        self.pos = {"x": pos[0], "y": pos[1], "z": pos[2]}
-        self.color = {"red": color[0], "green": color[1], "blue": color[2]}
-        self.size = size
-        self.theta = 0
-        if self.size is None:
-            self.size = {"length": 1, "width": 1, "height": 1}
-        else:
-            self.size = {"length": size[0], "width": size[1], "height": size[2]}
+        self.pos = vector(pos[0], pos[1], pos[2])  # {"x": , "y": , "z": }
+        self.color = vector(color[0], color[1], color[2])  # {"red": , "green": , "blue": }
+        self.vertex = vector(1, pos[1], pos[2])
 
-        self.vertex = vector(self.size["height"], self.pos["y"], self.pos["x"])
+        horizontal = pyramid(pos=self.pos, color=self.color)
+        vertical = pyramid(pos=vector(pos[0], pos[1]-1, pos[2]), color=self.color)
+        vertical.rotate(angle=math.radians(45), origin=self.vertex)
+        vertical.rotate(angle=math.radians(45), axis=self.vertex)
 
-        cubelet = pyramid(pos=vector(self.pos["x"], self.pos["y"], self.pos["z"]),
-                          color=vector(self.color["red"], self.color["green"], self.color["blue"]),
-                          size=vector(self.size["length"], self.size["width"], self.size["height"]))
+        """                               MAYBE LATER
+        if size is None:
+                    self.size = (1, 1, 1)  # {"length": 1, "width": 1, "height": 1}
+                else:
+                    self.size = vertex(size[0], size[1], size[2])
+        """
 
 
 Cubelet((0, 0, 0), (255, 255, 255))
